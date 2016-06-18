c1971e9d94e8c55a697a5ee534742557df913735
DocTyp <doctypgithub@gmail.com>
2016-06-19 00:26:34 +0200

    Update extension.js

--- src/extension.js
+++ src/extension.js
@@ -47,7 +47,7 @@
     'quote': {pattern: /[\{\}]/gm, regex: /\{{1,}([\s\S]*?)\}{1,}/gm}
   };
   var list = {
-    'unordered': {pattern: /\-/gm, regex: /((.*?)\-(?!\-).+$\n){1,}/gm},
+    'unordered': {pattern: /\-/gm, regex: /((.*?)\-(?!(.*?)\-).+$\n){1,}/gm},
     'ordered': {pattern: /\d{1,}\./gm, regex: /((.*?)\d{1,}\..+$\n){1,}/gm}
   };
   var link = {
