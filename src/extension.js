7b924347af3f8cf558842034269e73602446bf81
DocTyp <doctypgithub@gmail.com>
2016-06-18 22:28:54 +0200

    Update extension.js

--- src/extension.js
+++ src/extension.js
@@ -1,29 +1,5 @@
 (function(exports) {
   /*============================================================
-  ============================Loader============================
-  ============================================================*/
-  exports.LoadScript = function(url) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = 'https://doctyp.github.io/src/' + url;
-    document.getElementsByTagName('head')[0].appendChild(script);
-  };
-  exports.LoadStyle = function(url) {
-    var link = document.createElement('link');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.href = 'https://doctyp.github.io/src/' + url;
-    link.media = 'none';
-    link.onload = function() {
-      if (link.media != 'all') {
-        link.media = 'all';
-      }
-    };
-    document.getElementsByTagName('head')[0].appendChild(link);
-  };
-  
-  /*============================================================
   ============================Header============================
   ============================================================*/
   exports.Header = function(doc) {
