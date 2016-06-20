f9df10416c6736c613187ef0f067fce28d334748
DocTyp <doctypgithub@gmail.com>
2016-06-20 15:40:55 +0200

    Update extension.js

--- src/extension.js
+++ src/extension.js
@@ -281,7 +281,7 @@
     return doc.replace(/(^\s+|\s+$)/gm, '');
   };
   exports.Clean = function(doc) {
-    return doc;//.replace(/(\n|\<br\>){2,}/gm, '<br>');
+    return doc.replace(/\n/gm, '<br>');
   };
   
   /*============================================================
