52334ec696a1f9ef781a63136eb5f79722911303
DocTyp <doctypgithub@gmail.com>
2016-06-21 18:23:28 +0200

    Update DocTypBeta.js

--- src/DocTypBeta.js
+++ src/DocTypBeta.js
@@ -1,25 +1,5 @@
-var LoadScript = function(url) {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.async = true;
-  script.src = 'https://doctyp.github.io/src/' + url;
-  document.getElementsByTagName('head')[0].appendChild(script);
-};
-
-var LoadStyle = function(url) {
-  var link = document.createElement('link');
-  link.type = 'text/css';
-  link.rel = 'stylesheet';
-  link.href = 'https://doctyp.github.io/src/' + url;
-  link.media = 'none';
-  link.onload = function() {
-    if (link.media != 'all') {
-      link.media = 'all';
-    }
-  };
-  document.getElementsByTagName('head')[0].appendChild(link);
-};
-
-var DocTypBeta = (function(exports) {
-  return exports;
+var DocTypBeta = (function(E) {
+  var S = (E.Settings == true) ? Settings : throw('The Settings.js file did not load correctly!');
+  var M = (E.Modules == true) ? Modules : throw('The Modules.js file did not load correctly!');
+  return E;
 })({});
